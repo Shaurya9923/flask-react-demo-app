d2f283dde285de8861342f066ba9a24791766acd
Shaurya9923 <61802002+Shaurya9923@users.noreply.github.com>
2024-01-12 19:09:52 -0500

    Added an external api in react.

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@ import axios from 'axios';
 
 const App = () => {
   const [state, setState] = useState({});
+  const [repos , setRepos] = useState([]);
   const fetchMata = async ()=> {
     const result = await axios.get('/members')
     setState(result.data)
+  }
+  const fetchRepos = async ()=> {
+    const result = await axios.get(`https://api.github.com/users/Shaurya9923/repos`)
+    setRepos(result.data)
+    console.log(result.data)
   }
   useEffect(()=>{
     //fetch('/members').then(res=>{console.log(res)}).then(data=>{setState(data); console.log(data)})
     //fetchMata()
     console.log('fetching')
     // https://api.github.com/users/Shaurya9923/repos
-    // fetch(`/members`)
+    // axios(`/https://api.github.com/users/Shaurya9923/repos`)
     // .then(res => {res.json();console.log(res)})
     // .then(
     //   (result) => {
@@ -22,15 +28,23 @@ const App = () => {
     //     console.log(error)
     //   }
     // )
-    fetchMata(setState)
-  },[])
+    fetchMata()
+    fetchRepos()
+  }, [])
   return (
     <div>
       <h1>{state.members}</h1>
       Date Joined Date: {state.current_date} <br/>
+      Friends List :
       <ul>
         {state.friends && state.friends.map((members, index)=>{
           return <li key={index}>{members}</li>
+        })}
+      </ul>
+      Github Repo List :
+      <ul>
+        {repos && repos.map((repo, index)=>{
+          return <li key={index}>{repo.name}</li>
         })}
       </ul>
     </div>
